ce0d1433930435f339318908cadc622392754b50
Ayomiposi Aborisade <bobleeswaggs123@gmail.com>
2023-12-27 23:55:31 +0100

    third commit today

--- src/components/News.js
+++ src/components/News.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
 const News = (props) => {
+    const trimWords = (toBeTrimmed, words) => {
+        let trimedString = toBeTrimmed.trim()
+        if(trimedString.length > words){
+            return `${trimedString.slice(0, words-3)}...`
+        }else{
+            return toBeTrimmed
+        }
+    }
     const newsArray = props.articles.map((article) => (
-        <div className='sub_news rounded-xl' key={article.article_id}>
-            <img src= {article.urlToImage} alt='soft'/>
-            <div className='shadow-lg shadow-[#E2E7EB]'>
+        <div className='sub_news rounded-xl  h-full' key={article.article_id}>
+            <img className='rounded-t-xl' src= {article.urlToImage} alt='soft'/>
+            <div className='shadow-lg rounded-xl shadow-[#E2E7EB]'>
                 <div className='flex justify-between'>
                     <h5 className='text-sm text-[#485967]'>Date</h5>
                     <h5 className='text-sm text-[#2196F3]'>Corporate News</h5>
                 </div>
-                <h5>{article.title}</h5>
-                <p>{article.description}</p>
+                <h5 className='text-[red]'>{trimWords(article.title, 50)}</h5>
+                <p>{trimWords(article.description, 100)}</p>
                     <button onClick={() => props.checkId(article.article_id)}>Read More</button>
             </div>
         </div>
