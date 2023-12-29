6533076b7e924bb2ea571e0db606bec11e5eecee
Ayomiposi Aborisade <bobleeswaggs123@gmail.com>
2023-12-29 21:12:34 +0100

    Third commit today

--- src/pages/Newsfall.js
+++ src/pages/Newsfall.js
@@ -1,22 +1,29 @@
 import React from 'react'
 // import Split from 'react-split'
-
 const Newsfall = (props) => {
-  
+  const trimWords = (toBeTrimmed, words) => {
+    let trimedString = toBeTrimmed.trim()
+    if(trimedString.length > words){
+        return `${trimedString.slice(0, words-3)}...`
+    }else{
+        return toBeTrimmed;
+    }
+  }
   const newsfall = () => {
+    
     return (
-      <div>
+      <div className='m-2'>
         {props.updateData.map((article) => (// Using the newly created arry which i mapped through to create something new
-          <div key={article.article_id} className='flex '>
-            <div className='w-[200px] overflow-hidden'>
-              <img className='w-[80%] ' src={article.urlToImage} alt="Article" />
+          <div key={article.article_id} className='flex my-4 gap-2 shadow-lg rounded-xl shadow-[#E2E7EB]'>
+            <div className='w-[200px] h-[140px] overflow-hidden '>
+              <img className='w-[100%] h-full object-cover rounded-l-lg' src={article.urlToImage} alt="Article" />
             </div>
-            <div className='w-full'>
-              <h2>{article.title}</h2>
-              <p>{article.description}</p>
-              <button onClick={() => props.onclick(article.article_id, props.updateData, props.setClickedArticle)}>
-                Read More
-              </button>
+            <div className='w-full flex flex-col py-2 justify-between  '>
+              <h2>{trimWords(article.title, 50)}</h2>
+              <p className='text-[.8rem]'>{trimWords(article.description, 90)}  <button className='remove' onClick={() => props.onclick(article.article_id, props.updateData, props.setClickedArticle)}>
+                Read More &rarr;
+              </button></p>
+              
               {/* The reason for the arrow function before the function is to prevent calling the function on render, ensuring it's invoked only when called */}
             </div>
           </div>
@@ -32,7 +39,7 @@ const Newsfall = (props) => {
         <img src= {props.clickedArticle.urlToImage} alt='bollocks'/>
         <p>{props.clickedArticle.content}</p>
       </section>
-      <section className='bg-[red] sidebar'>
+      <section className=' sidebar'>
         <h1>HELLO WORLD</h1>
         {newsfall()}
       </section>
