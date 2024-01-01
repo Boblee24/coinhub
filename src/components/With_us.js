3fc21e7b3ddcf2bc9ea001adc8ad4506ab83d035
Boblee24 <112091786+Boblee24@users.noreply.github.com>
2024-01-01 01:00:07 -0800

    Update With_us.js

--- src/components/With_us.js
+++ src/components/With_us.js
@@ -3,7 +3,7 @@ import React from 'react'
 const WITH_US = () => {
   return (
     <div className='with_us text-center bg-[#EFF2F4] my-10 py-5 px-4'>
-        <h3>All of this and much more dedicated from <span>Tradiant</span> to your success</h3>
+        <h3>All of this and much more dedicated from <span>Tradiant</span> to your success.</h3>
         <button className='my-4'>Trade with us</button>
     </div>
   )
