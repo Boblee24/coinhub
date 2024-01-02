3ddecdbbb2af52af66d8a1bf9ed5a461fa011142
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-02 08:23:41 -0800

    third commit today

--- src/components/Home_hero.js
+++ src/components/Home_hero.js
@@ -3,15 +3,15 @@ import Images from '../Images'
 
 const HOME_HERO = () => {
   return (
-    <div className='bg-[#F5F5F5] home flex flex-col pt-14'>
+    <div className='bg-[#F5F5F5] home flex flex-col pt-14 font-[Poppins]'>
         <div className='m-3 p-2'>
             <div className=''>
-                <p><span className='text-[#91A3B2] font-[Poppins]' >Smart Investation</span></p>
-                <h2 className='text-[1.8rem]'>Invest Your Money With Us and <span className='home_hero_span'>Get Amazing Profit</span></h2>
-                <p className='my-4'>Maximize your investment in the capital market world with the convenience and various features provided by Tradiant.Aborisade Ayomiposi emmanuel</p>
+                <p><span className='text-[#91A3B2] ' >Smart Investation</span></p>
+                <h2 className='text-[1.8rem]   font-bold'>Invest Your Money With Us and <span className='text-[#2196F3]'>Get Amazing Profit</span></h2>
+                <p className='my-4 '>Maximize your investment in the capital market world with the convenience and various features provided by Tradiant.</p>
                 <div className='home_hero_links flex gap-4 items-center my-3'>
-                    <a href='/'>Get started</a>
-                    <a className='remove' href='/'>See How It Works &rarr;</a>
+                    <a href='/' className=''>Get started</a>
+                    <a className='remove ' href='/'>See How It Works &rarr;</a>
                 </div>
             </div>
             <div className='home_hero_left_sponsor'>
