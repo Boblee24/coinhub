b26280617766e2659abd9c75dfff23b5f55ed4c1
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-04 19:39:16 +0100

    fourth commit

--- src/components/Why_us_Why_us.js
+++ src/components/Why_us_Why_us.js
@@ -8,33 +8,33 @@ const WHY_US_WHY_US = () => {
   return (
     <div className='s my-6'>
         <div className='m-3'>
-            <div className='clild'>
+            <div className='flex flex-col gap-4 my-4'>
                 <div className='left'>
                     <video width="640" height="360" className="video-js vjs-default-skin vjs-big-play-centered rounded-lg"controls src= './video1.mp4' type="video/mp4"></video>
                 </div>
                 <div className=''>
-                    <h4>WHY US??</h4>
-                    <h3>A <span>Friendly Platform</span> for Beginner Traders</h3>
+                    <h4 className='font-[Poppins] text-[1.1rem] font-[500] text-[#2196F3]'>WHY US??</h4>
+                    <h3 className='font-[Epilogue] font-bold text-[1.8rem]'>A <span className='text-[#2196F3]'>Friendly Platform</span> for Beginner Traders</h3>
                     <p>Many people want to trade forex, but have a hard time understanding the basics and how to trade forex. Therefore, Tradiant presents an online-based interactive educational platform to help you learn to trade. The educational content is made light and concise so that a novice trader can learn to trade from scratch in 1 hour.</p>
-                    <button>Learn More</button>
+                    <button className='my-4'>Learn More</button>
                 </div> 
             </div>
-            <div className='child'>
+            <div className='flex flex-col gap-4 my-4'>
                 <div className=''>
                     <video width="640" height="360" className="video-js vjs-default-skin vjs-big-play-centered rounded-lg"controls src= './video2.mp4' type="video/mp4"></video>
                 </div>
                 <div className=''>
-                    <h4>WHY US??</h4>
-                    <h3><span>Additional Benefits</span> of Trading at Tradiant</h3>
+                    <h4 className='font-[Poppins] text-[1.1rem] font-[500] text-[#2196F3]'>WHY US??</h4>
+                    <h3 className='font-[Epilogue] font-bold text-[1.8rem]'><span className='text-[#2196F3]'>Additional Benefits</span> of Trading at Tradiant</h3>
                     <p>Every time you transact at Tradiant, you earn Tradiant Rewards points which can be exchanged for various travel, gadget, lifestyle, and automotive gifts. This program applies to all Live Standard Account customers. If trading on a Demo Account you don't get a prize, yes.</p>
-                    <button>Learn More</button>
+                    <button className='my-4'>Learn More</button>
                 </div> 
             </div>
         </div>
         <div className='additional text-center m-3'>
             <div className='additional_head'>
-                <h3>Additional advantages from <span className='text-[#2196F3]'>Tradiant</span></h3>
-                <p>At Tradiant, you will benefit from us, such as many conveniences in transactions, user-friendly transparency, and many others</p>
+                <h3 className='text-[1.8rem] font-[Epilogue] font-[700]'>Additional advantages from <span className='text-[#2196F3]'>Tradiant</span></h3>
+                <p className='text-[#91A3B2]'>At Tradiant, you will benefit from us, such as many conveniences in transactions, user-friendly transparency, and many others</p>
             </div>
             <div className='additional_flex flex flex-wrap gap-4 my-7'>
                 <div className='m-auto w-[300px] border-solid border-2 border-[#C7D0D8] px-4 py-2 rounded-lg shadow-lg shadow-[#E2E7EB]'>
