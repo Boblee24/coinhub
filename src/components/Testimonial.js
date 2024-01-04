ac0150823e8b547f41633795cf493ee5ea884b0a
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-04 19:45:42 +0100

    sixth commit

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -17,49 +17,49 @@ const Testimonial = () => {
   return (
     <div className='testimonial m-2 text-center'>
         <div className='top_value'>
-            <h3 className='text-[20]'>Testimonials</h3>
+            <h3 className='text-[1.5rem] font-[Epilogue] font-bold'>Testimonials</h3>
             <p>Millions of global traders have chosen Tradiant as their stock broker of choice and here are their thoughts after trading with Tradiant.</p>
         </div>
         <Slider {...settings}>
            <div className='bottom_value  my-2' >
               <div className='flex justify-center'><SiSimplenote className='text-[blue]' /></div>
               <div>
-                  <h5>Ayomiposi Aborisade</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Ayomiposi Aborisade</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
           <div className='bottom_value my-2'>
               <div className='flex justify-center'><SiSimplenote /></div>
               <div>
-                  <h5>Accesibilty</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Accesibilty</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
           <div className='bottom_value my-2'>
               <div><SiSimplenote /></div>
               <div>
-                  <h5>Simplicity</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Simplicity</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
           <div className='bottom_value my-2'>
               <div><SiSimplenote /></div>
               <div>
-                  <h5>Simplicity</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Simplicity</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
           <div className='bottom_value my-2'>
               <div><SiSimplenote /></div>
               <div>
-                  <h5>Simplicity</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Simplicity</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
           <div className='bottom_value my-2'>
               <div><SiSimplenote /></div>
               <div>
-                  <h5>Simplicity</h5>
+                  <h5 className='font-[Poppins] font-semibold text-[1.2rem]'>Simplicity</h5>
                   <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
