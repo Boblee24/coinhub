c8c84b9fc7efb29c226b376959b23144ba8c3291
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-05 19:58:28 +0100

    fourth commit

--- src/components/News.js
+++ src/components/News.js
@@ -12,15 +12,15 @@ const News = () => {
         }
     }
     const newsArray = updateData.map((article) => (
-        <div className='sub_news shadow-[#E2E7EB] shadow-lg rounded-xl self-stretch h-[355px] relative' key={article.article_id}>
+        <div className='sub_news shadow-[#E2E7EB] shadow-lg rounded-xl self-stretch h-[355px] relative my-2' key={article.article_id}>
             <img className='rounded-t-xl' src= {article.urlToImage} alt='soft'/>
-            <div className=' '>
+            <div className=' p-1'>
                 <div className='flex justify-between my-3'>
                     <h5 className='text-[.8rem] text-[#485967] '>21/03/2022</h5>
                     <h5 className='text-[.8rem] text-[#2196F3]'>Corporate News</h5>
                 </div>
                 <h5 className='text-[.9rem] font-[Poppins] font-semibold'>{trimWords(article.title, 50)}</h5>
-                <p className='font-[Poppins] text-[0.9rem]'>{trimWords(article.description, 100)}</p>
+                <p className='font-[Poppins] text-[0.8rem]'>{trimWords(article.description, 100)}</p>
                 <button className='absolute right-3 bottom-2 remove' onClick={() => checkId(article.article_id)}>Read More &rarr;</button>
             </div>
         </div>
