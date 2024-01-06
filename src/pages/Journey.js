8acc78d66a4d7dfdfc0b66731fe5d7c9cdf201bc
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-06 20:54:14 +0100

    Third commit

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -7,16 +7,16 @@ const Journey = () => {
   
   const journeyvideos = (heading1, heading2) => {
     return (
-      <div>
+      <div className='p-2'>
         <div className='text-center'>
-          <h2 className='text-[2.3rem]'>{heading1}</h2>
-          <p className='text-[#485967] text-[14px]'>{heading2}</p>
+          <h2 className='text-[2.1rem] font-[Epilogue] font-bold'>{heading1}</h2>
+          <p className='text-[#5D7285] text-[14px] font-[Poppins]'>{heading2}</p>
         </div>
-        <div className='grid grid-cols-1 gap-4 m-4 items-stretch'>
+        <div className='grid grid-cols-1 gap-4 m-2 items-stretch'>
           {VideoArray.videos.map((videoarray) => (
             <div key={videoarray.article_id}>
               <video width="640" height="360" className="video-js vjs-default-skin vjs-big-play-centered rounded-lg"controls src={videoarray.src} type="video/mp4"></video>
-              <h2>{videoarray.title}</h2>
+              <h2 className='font-[Poppins] font-semibold p-1'>{videoarray.title}</h2>
             </div>
           ))}
         </div>
