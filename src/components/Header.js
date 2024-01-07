814460ac588f261a50ff51fa3f32afb834e82566
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-07 20:56:07 +0100

    seventh

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,16 +16,16 @@ const Header = (props) => {
             <div>
                 <div>
                     <ul className='flex justify-center flex-col py-7 items-start font-[Poppins]'>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700 hover:text-red-700'  to="/about" onClick={props.handleHarmbugerclick}>About</NavLink></li>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/why_us" onClick={props.handleHarmbugerclick}>Why Us?</NavLink></li>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/blog" onClick={props.handleHarmbugerclick}>Blog</NavLink></li>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/journey" onClick={props.handleHarmbugerclick}>Journey</NavLink></li>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/help" onClick={props.handleHarmbugerclick}>Help</NavLink></li>
+                        <li className='w-[100%] text-[#485967] '><NavLink className='flex active:text-[#2196F3] active:font-semibold'  to="/about" onClick={props.handleHarmbugerclick}>About</NavLink></li>
+                        <li className='w-[100%] text-[#485967] '><NavLink className='flex active:text-[#2196F3] active:font-semibold'  to="/why_us" onClick={props.handleHarmbugerclick}>Why Us?</NavLink></li>
+                        <li className='w-[100%] text-[#485967] '><NavLink className='flex active:text-[#2196F3] active:font-semibold'  to="/blog" onClick={props.handleHarmbugerclick}>Blog</NavLink></li>
+                        <li className='w-[100%] text-[#485967] '><NavLink className='flex active:text-[#2196F3] active:font-semibold'  to="/journey" onClick={props.handleHarmbugerclick}>Journey</NavLink></li>
+                        <li className='w-[100%] text-[#485967] '><NavLink className='flex active:text-[#2196F3] active:font-semibold'  to="/help" onClick={props.handleHarmbugerclick}>Help</NavLink></li>
                     </ul>
                 </div>
                 <div className='flex gap-4 flex-col login text-center justify-between m-2'>
-                    <a className='' href='/'>Sign Up &rarr;</a>
-                    <a className='' href='/'>Login &rarr;</a>
+                    <a className='font-[Poppins]' href='/'>Sign Up &rarr;</a>
+                    <a className='font-[Poppins]' href='/'>Login &rarr;</a>
                 </div>
                 <div className='relative bg-[red]  inset-0'>
                     <div className='social absolute flex justify-between bottom-[6rem] h-full top-2/4 left-2/4' >
