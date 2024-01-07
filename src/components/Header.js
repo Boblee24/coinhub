75dc9f217539bcd23c3d05c292add400008ccd8e
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-07 20:45:52 +0100

    sixth

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
             <div>
                 <div>
                     <ul className='flex justify-center flex-col py-7 items-start font-[Poppins]'>
-                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/about" onClick={props.handleHarmbugerclick}>About</NavLink></li>
+                        <li className='w-[100%] '><NavLink className='flex active:text-violet-700 hover:text-red-700'  to="/about" onClick={props.handleHarmbugerclick}>About</NavLink></li>
                         <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/why_us" onClick={props.handleHarmbugerclick}>Why Us?</NavLink></li>
                         <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/blog" onClick={props.handleHarmbugerclick}>Blog</NavLink></li>
                         <li className='w-[100%] '><NavLink className='flex active:text-violet-700'  to="/journey" onClick={props.handleHarmbugerclick}>Journey</NavLink></li>
