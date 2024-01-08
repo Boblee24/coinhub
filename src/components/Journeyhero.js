1aeece6adb2a6e8f097b867f631e2cf1fa23e763
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-08 22:38:01 +0100

    fifth

--- src/components/Journeyhero.js
+++ src/components/Journeyhero.js
@@ -8,7 +8,7 @@ const Journeyhero = () => {
             <div className=''>
                 <h6 className='text-[#2196F3] py-2 font-[Poppins] font-semibold'>Learn With Tradiant</h6>
                 <h2 className='text-[2.5rem] leading-[3rem] font-[Epilogue] font-bold'><span className='text-[#2196F3]'>GOFX Tradiant</span>Trading Class Program</h2>
-                <p className='py-4 text-[#485967] font-[Poppins]'>We present this program for you, in order to prepare your skills, mentality, and technical knowledge well before starting trading.</p>
+                <p className='py-4 text-[#485967]  font-[Poppins]'>We present this program for you, in order to prepare your skills, mentality, and technical knowledge well before starting trading.</p>
                 <div className='journey_hero_links'>
                     <button className='my-4'>Start Journey</button>
                 </div>
