7294159db404cce17de658ecf0843b3cb8667e22
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 21:09:01 +0100

    whatever

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -22,13 +22,13 @@ const Brand = () => {
         <h2 className='text-[1.6rem] md:text-[24px] font-[Poppins] text-center font-semibold p-2'>Invest in their stock and be part of the business</h2>
         <div className='brands gap-4 items-center'>
               <Slider {...settings}>
-                <div className='p-3 md:p-7'><img className='' src={Images.segment} alt='segment'/> </div>
-                <div className='p-3 md:p-7'><img className='mt-1 md:mt-2' src={Images.oracle} alt='oracle'/></div>
-                <div className='p-3 md:p-7'><img className='' src={Images.protonet} alt='protonent'/></div>
-                <div className='p-3 md:p-7'><img className='mt-1' src={Images.samsung} alt='samsung'/></div>
-                <div className='p-3 md:p-7'><img className='' src={Images.morpheus} alt='morpheus'/></div>
-                <div className='p-3 md:p-7'><img className='' src={Images.monday} alt='monday'/></div>
-                <div className='p-3 md:p-7'><img className='' src={Images.open} alt='open'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] ' src={Images.segment} alt='segment'/> </div>
+                <div className='p-3 md:p-7'><img className='px-[10%] mt-1 md:mt-2' src={Images.oracle} alt='oracle'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] ' src={Images.protonet} alt='protonent'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] mt-1' src={Images.samsung} alt='samsung'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] ' src={Images.morpheus} alt='morpheus'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] ' src={Images.monday} alt='monday'/></div>
+                <div className='p-3 md:p-7'><img className='px-[10%] ' src={Images.open} alt='open'/></div>
               </Slider>
         </div>
     </div>
