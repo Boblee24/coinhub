4942244ff5bedfebf197edb63fa31d4dc0ee7ced
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 17:03:59 +0100

    fifth today, softtt ba??

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,11 +4,14 @@ import Images from '../Images'
 const Features = () => {
   return (
     <div className='feature'>
-        <div className='flex flex-col m-2'>
-            <div className='p-5 w-[80%] m-auto'><img className='w-[80%]' src={Images.girlwithphone} alt='girlwith'/></div>
-            <div className='right'>
-                <h4 className='text-[#2196F3] font-semibold'>OUR FEATURES</h4>
-                <h3 className='text-[1.5rem] font-bold'>Receive update market quickly everytime</h3>
+        <div className='flex flex-col m-2 md:flex-row md:justify-between md:items-center'>
+            <div className="p-5  m-auto relative z-[10] md:after:content-['x'
+        ] md:after:absolute md:after:z-[-1] md:after:bottom-5 md:after:bg-slate-400 md:after:w-[100%] md:after:h-[80%] md:after:rounded-xl " >
+                <img className='' src={Images.girlwithphone} alt='girlwith'/>
+            </div>
+            <div className='w-[60%] md:ml-[4rem]'>
+                <h4 className='text-[#081520] md:text-[1.2rem] font-semibold'>OUR FEATURES</h4>
+                <h3 className='text-[1.5rem] md:text-[2rem] font-bold'>Receive update market quickly everytime</h3>
                 <p className='my-4 text-[#5D7285]'>Equipped with comprehensive analysis tools and Artificial Intelligent, it makes it easier and gives you a profitable experience in the form of market updates quickly everytime, so it is not too late to make the best investment decisions.</p>
                 <button className='text-[#fff]'>Get Started</button>
             </div> 
--- src/components/Helphero.js
+++ src/components/Helphero.js
@@ -6,14 +6,14 @@ const Helphero = () => {
   return (
     <div className='helphero relative bg-no-repeat bg-cover h-[100dvh] m-0 p-0 bg-center' style={{backgroundImage:`url(${Images.help_hero})`}}>
         <div className="about_hero_note flex justify-right items-center h-full p-2 z-20">
-          <div className='help_hero_note'>
+          <div className='max-w-md'>
               <h4 className='text-[#2196F3] text-[1.1rem font-medium] py-4 font-[Poppins]' >Support Center</h4>
               <h2 className='text-[2rem] font-[Epilogue] font-bold'>Welcome to the Tradiant Support Center, Can I help you?</h2>
               <label className="relative block my-3">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <div className="h-5 w-5 fill-slate-300"><CiSearch/></div>
                 </span>
-                <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+                <input className="placeholder:italic  placeholder:text-slate-400 block bg-white w-[50%] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
               </label>
           </div>
         </div>
