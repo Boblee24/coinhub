5d417f78db8ddd245226c24a290cd3d313dd8e12
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 17:41:10 +0100

    sixth today uhn

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,24 +4,23 @@ import Images from '../Images'
 const Features = () => {
   return (
     <div className='feature'>
-        <div className='flex flex-col m-2 md:flex-row md:justify-between md:items-center'>
-            <div className="p-5  m-auto relative z-[10] md:after:content-['x'
-        ] md:after:absolute md:after:z-[-1] md:after:bottom-5 md:after:bg-slate-400 md:after:w-[100%] md:after:h-[80%] md:after:rounded-xl " >
+        <div className='flex flex-col m-2 md:flex-row md:justify-between md:items-center gap-3 md:gap-7 md:px-[3%] py-3'>
+            <div className="p-5  m-auto relative z-[10] md:after:content-[''] md:after:absolute md:after:z-[-1] md:after:bottom-5 md:after:bg-slate-400 md:after:w-[100%] md:after:h-[80%] md:after:rounded-xl" >
                 <img className='' src={Images.girlwithphone} alt='girlwith'/>
             </div>
-            <div className='w-[60%] md:ml-[4rem]'>
-                <h4 className='text-[#081520] md:text-[1.2rem] font-semibold'>OUR FEATURES</h4>
+            <div className='w-[55%] md:ml-[4rem]'>
+                <h4 className='text-[#2196F3] md:text-[1.2rem] font-semibold'>OUR FEATURES</h4>
                 <h3 className='text-[1.5rem] md:text-[2rem] font-bold'>Receive update market quickly everytime</h3>
                 <p className='my-4 text-[#5D7285]'>Equipped with comprehensive analysis tools and Artificial Intelligent, it makes it easier and gives you a profitable experience in the form of market updates quickly everytime, so it is not too late to make the best investment decisions.</p>
                 <button className='text-[#fff]'>Get Started</button>
             </div> 
         </div>
-        <div className='flex flex-col m-2'>
-            <div className='my-3'><img className='rounded-lg' src={Images.videosystem} alt='videosystem'/></div>
-            <div className='right'>
-                <h4 className='text-[#2196F3] font-semibold'>OUR FEATURES</h4>
-                <h3 className='text-[1.5rem] font-bold'>Receive update market quickly everytime</h3>
-                <p className='my-4 text-[#5D7285]'>Equipped with comprehensive analysis tools and Artificial Intelligent, it makes it easier and gives you a profitable experience in the form of market updates quickly everytime, so it is not too late to make the best investment decisions.</p>
+        <div className='flex flex-col m-2 md:flex-row-reverse md:justify-between md:items-center gap-3 md:gap-7 md:px-[3%] py-3'>
+            <div className='my-3'> <img className='rounded-lg' src={Images.videosystem} alt='videosystem'/> </div>
+            <div className='w-[53%]'>
+                <h4 className='text-[#2196F3] font-semibold md:text-[1.2rem] '>OUR FEATURES</h4>
+                <h3 className='text-[1.5rem] md:text-[2rem] font-bold'>Easy Way to Invest and Get Profit</h3>
+                <p className='my-4 text-[#5D7285]'>Using the Tradiant Application is an easy way to invest and benefit, equipped with comprehensive analysis tools and Artificial Intelligent (AI) that helps automation in monitoring and transactions..</p>
                 <button className='text-[#fff]'>Get Started</button>
             </div> 
         </div>
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='newsletter flex flex-col bg-[#CFE8FC] my-7 p-3 rounded-lg'>
+    <div className='newsletter flex flex-col bg-[#CFE8FC] my-7 p-3 rounded-lg md:m-[2%]'>
         <div className='my-4'>
             <h6 className=' text-[#2196F3]'>OUR NEWSLETTER</h6>
             <h3 className='leading-9 my-3'>Stay updated with our weekly newsletter</h3>
