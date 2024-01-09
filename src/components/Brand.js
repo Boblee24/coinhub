6f4a4d7000cd0ac677707387f0b7054f885d7e86
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 20:13:23 +0100

    seventh

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -19,16 +19,16 @@ const Brand = () => {
   };
   return (
     <div>
-        <h2 className='text-[1.6rem] text-center  font-[500] p-2'>Invest in their stock and be part of the business</h2>
-        <div className='brands gap-4'>
+        <h2 className='text-[1.6rem] md:text-[24px] text-center font-[500] p-2'>Invest in their stock and be part of the business</h2>
+        <div className='brands gap-4 items-center'>
               <Slider {...settings}>
-                <div className='p-3'><img className='' src={Images.segment} alt='segment'/> </div>
-                <div className='p-3'><img className='mt-1' src={Images.oracle} alt='oracle'/></div>
-                <div className='p-3'><img className='' src={Images.protonet} alt='protonent'/></div>
-                <div className='p-3'><img className='mt-1' src={Images.samsung} alt='samsung'/></div>
-                <div className='p-3'><img className='' src={Images.morpheus} alt='morpheus'/></div>
-                <div className='p-3'><img className='' src={Images.monday} alt='monday'/></div>
-                <div className='p-3'><img className='' src={Images.open} alt='open'/></div>
+                <div className='p-3 md:p-7'><img className='' src={Images.segment} alt='segment'/> </div>
+                <div className='p-3 md:p-7'><img className='mt-1 md:mt-2' src={Images.oracle} alt='oracle'/></div>
+                <div className='p-3 md:p-7'><img className='' src={Images.protonet} alt='protonent'/></div>
+                <div className='p-3 md:p-7'><img className='mt-1' src={Images.samsung} alt='samsung'/></div>
+                <div className='p-3 md:p-7'><img className='' src={Images.morpheus} alt='morpheus'/></div>
+                <div className='p-3 md:p-7'><img className='' src={Images.monday} alt='monday'/></div>
+                <div className='p-3 md:p-7'><img className='' src={Images.open} alt='open'/></div>
               </Slider>
         </div>
     </div>
--- src/components/Home_hero.js
+++ src/components/Home_hero.js
@@ -3,11 +3,11 @@ import Images from '../Images'
 
 const HOME_HERO = () => {
   return (
-    <div className='bg-[#F5F5F5] home flex flex-col pt-14 font-[Epilogue]'>
-        <div className='m-3 p-2'>
+    <div className='bg-[#F5F5F5] home flex flex-col pt-14 font-[Epilogue] md:flex-row md:items-center md:overflow-hidden'>
+        <div className='m-3 p-2 md:mr-0'>
             <div className='max-w-md'>
-                <p><span className='text-[#91A3B2] ' >Smart Investation</span></p>
-                <h2 className='text-[1.8rem] font-bold max-w-md '>Invest Your Money With Us and <span className='text-[#2196F3] xs:text-red-800'>Get Amazing Profit</span></h2>
+                <p><span className='text-[#91A3B2] md:text-[20px]' >Smart Investation</span></p>
+                <h2 className='text-[1.8rem] font-bold max-w-md md:text-[2.5rem] '>Invest Your Money With Us and <span className='text-[#2196F3]'>Get Amazing Profit</span></h2>
                 <p className='my-4 '>Maximize your investment in the capital market world with the convenience and various features provided by Tradiant.</p>
                 <div className='home_hero_links flex gap-4 items-center my-3'>
                     <a href='/' className=''>Get started</a>
@@ -21,8 +21,8 @@ const HOME_HERO = () => {
                 </div>
             </div>
         </div>
-        <div className='my-4'>
-            <img src={Images.home} alt='home'/>
+        <div className='my-4 md:w-[80%] mr-[-2rem] '>
+            <img className='md:w-[90%]' src={Images.home} alt='home'/>
         </div>
     </div>
   )
