afe5c6eecfc4350c8a3becf65ce3d8b515ffe917
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 21:33:16 +0100

    whatever

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -6,10 +6,10 @@ const Partner = () => {
   return (
     <div className='partner m-2 '>
         <div className='partner_note text-center'>
-            <h3 className='font-[Epilogue] text-[1.6rem] font-[500]'>A trading partner you can trust</h3>
-            <p className='text-[.9rem]'>At Tradiant, we rigorously focus on quality and transparency. Our order execution quality is independently monitored. Order execution with high quality means: you save money with every investment you make with Tradiant.</p>
+            <h3 className='font-[Epilogue] text-[1.6rem] font-bold md:text-[40px]'>A trading partner you can trust</h3>
+            <p className='text-[.9rem] md:text-[1rem]'>At Tradiant, we rigorously focus on quality and transparency. Our order execution quality is independently monitored. Order execution with high quality means: you save money with every investment you make with Tradiant.</p>
         </div>
-        <div className='partners my-5 flex justify-between flex-col items-center text-center'>
+        <div className='partners my-5 flex justify-between flex-col items-center text-center md:flex-row md:gap-[10%] md:mx-[5%]'>
             <div className='partner_child my-5'>
                 <div className='flex justify-center pb-4 items-center'>
                     <IoFlashOutline className=' w-14 h-14 text-[#2196F3]' />
--- src/components/Vismis.js
+++ src/components/Vismis.js
@@ -3,18 +3,18 @@ import Images from '../Images'
 const Vismis = () => {
   return (
     <div className='Vismis m-2'>
-        <div className='flex flex-col gap-4'>
-            <div className='left'> <img src={Images.mission} alt='mission'/></div>
-            <div className=''>
-                <h3 className='text-[1.7rem] font-[Epilogue] font-[600]'>Our Vision</h3>
+        <div className='flex flex-col gap-4 md:flex-row  justify-between md:p-[4%]'>
+            <div className='md:w-[45%]'> <img src={Images.mission} alt='mission'/></div>
+            <div className='w-[45%]'>
+                <h3 className='text-[1.7rem] font-[Epilogue] font-[600] md:font-bold md:text-[40px]'>Our Vision</h3>
                 <p>Our vision is to become a trusted advisor, intermediary, and partner to assist customers in determining financial strategies in the capital market and Become a professional reinsurance broker with a global reputation.</p>
                 <button className='my-2 remove text-[.9rem]'>See More &rarr;</button>
             </div> 
         </div>
-        <div className='flex flex-col gap-4'>
-            <div className='left'> <img src={Images.vision} alt='vision'/></div>
-            <div className=''>
-                <h3 className='text-[1.7rem] font-[Epilogue] font-[600]'>Our Mission</h3>
+        <div className='flex flex-col gap-4 md:flex-row-reverse  justify-between md:p-[4%]'>
+            <div className='md:w-[45%]'> <img src={Images.vision} alt='vision'/></div>
+            <div className='md:w-[45%]'>
+                <h3 className='text-[1.7rem] font-[Epilogue] font-[600] md:font-bold md:text-[40px]'>Our Mission</h3>
                 <p>Providing excellent service from risk placement to settlement of compensation in the interest of obtaining the best capital market results. Maintain the profitability of each user with the aim of meeting the expectations of each user.</p>
                 <button className='my-2 remove text-[.9rem]'>See More  &rarr;</button>
             </div> 
