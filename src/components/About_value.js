20a603eae3b9580afec30ae0bd41599cb75c8a6c
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-09 21:50:35 +0100

    softttt

--- src/components/About_value.js
+++ src/components/About_value.js
@@ -16,12 +16,12 @@ const ABOUT_VALUE = () => {
   
   return (
     <div className='about_value bg-[#EFF2F4] py-7'>
-        <div className=' text-center'>
-            <h3 className='text-[1.8rem] font-[Epilogue] font-[700]'>Our Values</h3>
+        <div className=' text-center md:mx-[10%]'>
+            <h3 className='text-[1.8rem] font-[Epilogue] font-bold md:text-[40px]'>Our Values</h3>
             <p className='p-3 text-[.9rem]'>Our values are reflected in our product offering and user experience, as well as being embedded in our internal culture and employee experience.</p>
         </div>
         <Slider {...settings}>
-          <div className='bottom_value text-center p-3'>
+          <div className='bottom_value text-center p-3 md:px-[13%] md:my-5'>
               <div className='flex justify-center items-center'>
                 <SiEasyeda size={40} color="#28a745" />
               </div>
@@ -30,7 +30,7 @@ const ABOUT_VALUE = () => {
                     <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
-          <div className='bottom_value text-center p-3'>
+          <div className='bottom_value text-center p-3 md:px-[13%] md:my-5'>
               <div className='flex justify-center items-center'>
                 <SiEasyeda size={40} color="#28a745" />
               </div>
@@ -39,7 +39,7 @@ const ABOUT_VALUE = () => {
                     <p>We strive for simplicity in all things: our product, our marketing, our processes, our business model. The result is solutions that are beautiful in their efficiency and clarity, easier to understand wholly, easier to translate to different situations and scales, and easier to change later.</p>
               </div>
           </div>
-          <div className='bottom_value text-center p-3'>
+          <div className='bottom_value text-center p-3 md:px-[13%] md:my-5'>
               <div className='flex justify-center items-center'>
                 <SiEasyeda size={40} color="#28a745" />
               </div>
--- src/components/With_us.js
+++ src/components/With_us.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const WITH_US = () => {
   return (
-    <div className='with_us text-center  my-10 py-5 px-3'>
-        <h3 className='font-[Epilogue] font-[700] text-[1.7rem]'>All of this and much more dedicated from <span className='text-[#2196F3]'>Tradiant</span> to your success.</h3>
+    <div className='with_us text-center my-10 py-5 px-3 md:px-[10%]'>
+        <h3 className='font-[Epilogue] font-bold text-[1.7rem] md:text-[40px]'>All of this and much more dedicated from <span className='text-[#2196F3]'>Tradiant</span> to your success.</h3>
         <button className='my-4'>Trade with us</button>
     </div>
   )
