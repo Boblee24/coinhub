ea470bb9bb5c06441612c2323286e39c0d2aef85
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-11 17:54:50 +0100

    c

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex item-center justify-center p-1'>
-        <h6 className='text-[.7rem] p-2'> Copyright @Ayomiposi 2024. All Rights Reserved</h6>
+        <h6 className='text-[.7rem] font-[Poppins] font-semibold p-2 xs:text-[0.9rem] sm:text-[1rem]'> Copyright @Ayomiposi 2024. All Rights Reserved</h6>
     </div>
   )
 }
