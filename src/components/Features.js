2dfff4249152c37ebd5bfa9cdf56c2f7bb732122
Aborisade Ayomiposi <bobleeswaggs123@gmail.com>
2024-01-13 20:08:44 +0100

    sha commit

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Images from '../Images'
 
-const Features = () => {//helo worll
-  return (//my name is aborisade ayomiposi so
+const Features = () => {
+  return (
     <div className='feature'>
         <div className='flex flex-col m-2 md:ml-9 md:flex-row md:justify-between md:items-center gap-3 md:gap-7 md:px-[3%] py-3 '>
             <div className="p-5 w-[60%] m-auto sm:m-0 md:w-[30%] relative z-[10] md:after:content-[''] md:after:absolute md:after:z-[-1] md:after:bottom-5 md:after:left-[-2rem] md:after:bg-slate-400 md:after:w-[calc(140%)] md:after:h-[70%] md:after:rounded-xl" >
